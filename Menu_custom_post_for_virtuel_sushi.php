38076d10997b96170ac3d8dc6b5e71e24251c0d9
VirtuelDesign <41791879+VirtuelDesign@users.noreply.github.com>
2018-07-27 15:32:10 +0200

    Add files via upload

---
+++ Menu_custom_post_for_virtuel_sushi.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Menu Custom Post For Virtuel Sushi
+Description: Allow you to display the menu custom post for the portfolio page only for the theme Virtuel Sushi
+Author: Virtuel Design
+*/
+
+require_once plugin_dir_path(__FILE__) . 'mcpfvs-functions.php';
